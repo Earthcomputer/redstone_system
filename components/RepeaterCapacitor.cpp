df79d353ef310c142c478223d7d03d0144c40230
Earthcomputer <burtonjae@hotmail.co.uk>
2018-12-12 12:23:26 +0000

    RepeaterCapacitor

--- components/RepeaterCapacitor.cpp
+++ components/RepeaterCapacitor.cpp
@@ -12,40 +12,96 @@ RepeaterCapacitor::RepeaterCapacitor() {
     field_60 = 0;
     field_64 = false;
     field_65 = false;
-    // field_68
+    field_68 = new CircuitComponentList;
     for (int i = 0; i < 4; i++)
-        gap4_64[i] = 2;
-}
-
-RepeaterCapacitor::~RepeaterCapacitor() {
-    // TODO: impl
+        field_44[i] = 2;
 }
 
 bool RepeaterCapacitor::removeSource(const BlockPos *pos, const BaseCircuitComponent *component) {
-    // TODO: impl
+    bool baseRemoved = BaseCircuitComponent::removeSource(pos, component);
+    bool repeaterRemoved = field_68->removeSource(pos, component);
+    return baseRemoved || repeaterRemoved;
 }
 
 bool RepeaterCapacitor::addSource(CircuitSceneGraph *graph, const CircuitTrackingInfo *trackingInfo, int *a4,
                                   bool *a5) {
-    // TODO: impl
+    bool sameDir = trackingInfo->field_0.field_14 == getDirection();
+    bool oppositeDir = trackingInfo->field_0.field_14 == Facing::OPPOSITE_FACING[getDirection()];
+    if ((trackingInfo->field_40.field_18 != TYPE_POWERED_BLOCK || (trackingInfo->field_40.field_0->hasDirectPower() && *a5)) && !sameDir) {
+        ComponentType componentType = trackingInfo->field_40.field_0->getInstanceType();
+        if (oppositeDir || componentType == TYPE_REPEATER || componentType == TYPE_COMPARATOR)
+            trackPowerSource(trackingInfo, *a4, *a5, oppositeDir);
+    }
+    return false;
 }
 
 bool RepeaterCapacitor::allowConnection(CircuitSceneGraph *graph, const CircuitTrackingInfo *trackingInfo, bool *a4) {
-    // TODO: impl
+    return trackingInfo->field_0.field_14 == Facing::OPPOSITE_FACING[getDirection()];
 }
 
 void RepeaterCapacitor::checkLock() {
-    // TODO: impl
+    field_5E = false;
+    for (auto &item : *field_68) {
+        int v10 = item.field_0->getStrength() - item.field_8;
+        v10 = std::max(v10, 0);
+        if (v10 > 0) {
+            field_5E = true;
+            for (int i = 1; i < 4; i++) {
+                field_44[i] = field_44[0];
+            }
+            return;
+        }
+    }
 }
 
 bool RepeaterCapacitor::evaluate(CircuitSystem *system, const BlockPos *pos) {
-    // TODO: impl
+    bool oldVal = field_5C;
+    if (!field_5E) {
+        delayPulse(field_5D);
+        if (field_58 > 0) {
+            if (field_60 > field_58 && !(field_64 && field_60 == 2)) {
+                alternatePulse();
+            } else {
+                extendPulse();
+            }
+        }
+        field_5C = field_44[0] == 1 || field_44[0] == 3;
+    }
+    return oldVal != field_5C;
 }
 
 void RepeaterCapacitor::cacheValues(CircuitSystem *system, const BlockPos *pos) {
-    // TODO: impl
+    field_5D = false;
+    for (auto &item : *field_8) {
+        int v9 = item.field_0->getStrength() - item.field_8;
+        v9 = std::max(v9, 0);
+        if (v9 > 0) {
+            field_5D = true;
+            break;
+        }
+    }
+
+    if (!field_5E) {
+        if (field_64 == field_5D) {
+            field_60 = 0;
+        } else {
+            field_60++;
+            if (field_60 == 1) {
+                field_65 = !field_64;
+            }
+            field_64 = !field_64;
+        }
+    }
 }
 
 void RepeaterCapacitor::updateDependencies(CircuitSceneGraph *graph, const BlockPos *pos) {
-    // TODO: impl
+    for (auto it = field_8->begin(); it != field_8->end();) {
+        if (it->field_1C == 1) {
+            ++it;
+        } else {
+            BlockPos tmp = it->field_C;
+            field_68->add(it->field_0, it->field_8, &tmp);
+            it = field_8->erase(it);
+        }
+    }
 }
--- components/RepeaterCapacitor.h
+++ components/RepeaterCapacitor.h
@@ -10,20 +10,22 @@
 
 class RepeaterCapacitor : public CapacitorComponent {
 private:
+    int field_44[5];
     int field_58;
     bool field_5C;
-    bool field_5D;
+    bool field_5D; // TODO: what type should this be? delayPulse() suggests it's not a bool, but alignment suggests it's not an int
     bool field_5E;
     int field_60;
-    bool field_64;
+    bool field_64; // TODO: this is compared to field_5D, probably the same type
     bool field_65;
     CircuitComponentList *field_68;
-    int gap4_64[5];
 public:
     RepeaterCapacitor();
 
     // VTABLE #1
-    ~RepeaterCapacitor() override;
+    ~RepeaterCapacitor() override {
+        delete field_68;
+    }
 
     // VTABLE #2
     int getStrength() override {
@@ -34,10 +36,10 @@ public:
     void setStrength(int strength) override {
         field_5C = strength != 0;
         for (int i = 0; i < field_58 + 1; i++) {
-            gap4_64[i] = field_5C ? 3 : 2;
+            field_44[i] = field_5C ? 3 : 2;
         }
         for (int i = field_58 + 1; i <= 4; i++) {
-            gap4_64[i] = 0;
+            field_44[i] = 0;
         }
     }
 
@@ -80,6 +82,53 @@ public:
     // VTABLE #24
     Facing::Facing getPoweroutDirection() override {
         return Facing::OPPOSITE_FACING[getDirection()];
+    }
+
+    void delayPulse(int a2) {
+        for (int i = 1; i <= field_58; i++) {
+            field_44[i - 1] = field_44[i];
+        }
+        for (int i = field_58; i < 4; i++) {
+            field_44[i] = a2;
+        }
+    }
+
+    void alternatePulse() {
+        if (field_58 > 0) {
+            if (field_44[0] == 0 || field_44[0] == 1) {
+                int v1 = field_65 ? 3 : 2;
+                field_65 = !field_65;
+                for (int i = 0; i < field_58 + 1; i++) {
+                    field_44[i] = v1;
+                }
+            }
+        }
+    }
+
+    void extendPulse() {
+        if (field_58 > 0) {
+            if (field_44[0] == 2 && field_44[1] == 0) {
+                field_44[1] = 2;
+            } else if (field_44[0] == 3 && field_44[1] == 1) {
+                field_44[1] = 3;
+            }
+            if (field_44[0] == 0 || field_44[0] == 1) {
+                int v5 = 0;
+                for (int i = 0; i <= field_58; i++) {
+                    if (field_44[i] == field_44[0])
+                        v5++;
+                }
+                int v3;
+                if (field_44[0] != 0) {
+                    v3 = 3;
+                } else {
+                    v3 = v5 <= field_58 ? 3 : 2;
+                }
+                for (int i = 0; i <= field_58; i++) {
+                    field_44[i] = v3;
+                }
+            }
+        }
     }
 };
 
